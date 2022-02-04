69d41259956f81798dc21b364e3fe4eb84aa2ade
Josh <aftershock840@gmail.com>
2022-02-04 06:53:07 -0500

    re-work

--- server.js
+++ server.js
@@ -1,43 +1,51 @@
 // Require Express to run server and routes
+// var name from lesson 2.1
 const express = require('express');
 // Start up an instance of app
+// var name from lesson 2.1
 const app = express();
 // empty js object to be used
-const projectData = {};
+  const projectData = [];
 /* Dependencies */
+// from lesson 2.1
 const bodyParser = require('body-parser')
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+// from lesson 2.1
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // Cors for cross origin allowance
+// from lesson 2.1
 const cors = require('cors');
 app.use(cors());
 // Initialize the main project folder
+
 app.use(express.static('website'));
 // Setup Server
+// lesson 2.6
 const port = 8000;
 const server = app.listen(port, listening);
 // listen for server to start and console.log it
+// lesson 2.6
 function listening(){
     console.log("server running");
     console.log(`running on localhost: ${port}`);
 }
 // get request
-app.get('/all', function (req, res) {
+// lesson 3.3
+app.get('/get', function (req, res) {
   res.send(projectData)
 })
 // POST request
-app.post('/addData', addData)
+// lesson 3.4
+app.post('/post', postData)
 
-function addData(req,res){
+function postData(req,res){
 
-    newEntry = {
+    entryFormat = {
     temp: req.body.temp,
     date: req.body.date,
     feelings: req.body.feelings
     }
-    projectData.push(newEntry)
-    console.log(projectData)
-    res.send(projectData)
-  }
+    projectData.push(entryFormat);
+  };
