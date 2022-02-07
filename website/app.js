b847637e20ed48af72fa5114cf72c8c6bcf46726
Josh <aftershock840@gmail.com>
2022-02-07 04:13:48 -0500

    fixed incorrect month output/ updateUI function

--- website/app.js
+++ website/app.js
@@ -2,18 +2,18 @@
 const zip=  document.getElementById('zip').value;
 // url and api key
 const url = 'http://api.openweathermap.org/data/2.5/weather?zip=';
-const key = '&APPID=7ba3097fa4618bff87a065e43d2562b1&units=imperial';
+const apiKey = '&APPID=7ba3097fa4618bff87a065e43d2562b1&units=imperial';
 // Create a new date instance dynamically with JS
 const d = new Date();
-const newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+const newDate = d.getMonth() + 1 +'.'+ d.getDate()+'.'+ d.getFullYear();
 // feeling, zip, and date variables below
 const getValues = (e) => {
   const feelings = document.getElementById('feelings').value;
   const zip =  document.getElementById('zip').value;
-  const newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+  const newDate = d.getMonth() + 1 +'.'+ d.getDate()+'.'+ d.getFullYear();
 // post data to the object
 // lesson 4.9 & lesson 4.10
-  dataToObject(url , zip , key )
+  dataToObject(url , zip , apiKey )
   .then (function(data) {
       post('/post' ,{temp:data.main.temp ,date:newDate, feelings: feelings} )
   })
@@ -24,11 +24,12 @@ const getValues = (e) => {
 document.getElementById('generate').addEventListener('click', getValues);
 // GET data from API function
 // lesson 4.2
-const dataToObject = async (url, zip, key)=>{
-  const res = await fetch(url + zip + key)
+const dataToObject = async (url, zip, apiKey)=>{
+  const res = await fetch(url + zip + apiKey)
   try {
     const data = await res.json();
-    console.log(data)
+    // Uncomment console.log below to see full object
+    // console.log(data)
     return data;
   } catch(error) {
     console.log("error", error);
@@ -65,12 +66,11 @@ const updateUI = async() => {
   const req = await fetch('/get');
   try {
       const compData = await req.json();
-      const last = compData[compData.length-1];
       // console.log(compData);
       // update new entry values
-          document.getElementById('date').innerHTML = `date: ${last.date}`;
-          document.getElementById('temp').innerHTML = `Temperature: ${last.temp}`;
-          document.getElementById('content').innerHTML = `feeling: ${last.feelings}`;
+          document.getElementById('date').innerHTML = `Date: ${compData.date}`;
+          document.getElementById('temp').innerHTML = 'Temperature: ' + Math.round(compData.temp) + ' degrees';
+          document.getElementById('content').innerHTML = `Feeling: ${compData.feelings}`;
   } catch (error) {
       console.log('error', error);
   }
